95be40f94c3724ccacda440697178d4abca18384
Denis Lebedev <lebedev.dm00@mail.ru>
2024-05-31 20:18:29 +0900

    Add basic structure of Contoller, View

---
+++ src/controller/Controller.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.Model;
+
+public class Controller {
+  private Model model;
+  
+  public void setModel(Model model) {
+    this.model = model;
+  }
+  
+  public void onShowAllUsers() {
+    model.loadUsers();
+  }
+}
